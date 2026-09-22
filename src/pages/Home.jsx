6718934a0856f763f7560eb1c5ccbd0c9d73e653
secsmp <secsmp007@gmail.com>
2026-09-23 03:55:40 +0530

    Test live deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -72,7 +72,6 @@ useEffect(() => {
             <div className="hero-badge">
               <span className="status-dot"></span>
               IT Infrastructure & Digital Solutions
-              Samir Patni
             </div>
             
             <a href="/contact" className="health-checkup-cta">
